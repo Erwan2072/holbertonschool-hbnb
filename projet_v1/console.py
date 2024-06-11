706550f553f3a8e880ed94558ef3493e07c7d314
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-06-11 16:05:13 +0200

    modif dockerfile and console

--- projet_v1/console.py
+++ projet_v1/console.py
@@ -25,7 +25,7 @@ ns.add_namespace(api_city, path='/cities')
 import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
 
 #Docker
